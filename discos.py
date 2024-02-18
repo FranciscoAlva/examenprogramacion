7adbdf3c0e930a2d770ab2453d1050d593e55018
FranciscoAlva <FALVAREZR91@alumnos.imf.com>
2024-02-18 02:13:29 +0100

    if elif menu

--- discos.py
+++ discos.py
@@ -1,3 +1,4 @@
+#menu
 def menu():
     print("Escoge una opcion: ")
     print("1.-Insertar un registro")
@@ -6,8 +7,20 @@ def menu():
     print("4.-Eliminar un registro")
     print("5.-Buscar un registro")
     print("6.-Salir")
-    input("Escoge una opcion: ")
+    opcion = input("Escoge una opcion: ")
+    #if elif
+    if opcion == "1":
+        print("Insertamos un registro")
+    elif opcion == "2":
+        print("Mostramos los registros")
+    elif opcion == "3":
+        print("Modificamos los registros")
+    elif opcion == "4":
+        print("Eliminamos los registros")
+    elif opcion == "5":
+        print("Buscamos los registros")
+    elif opcion == "6":
+        print("Salimos del programa")
     menu()
-
 menu()
     
