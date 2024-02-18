f859ea3b5b8c4801f4ae3ef20b661d2b078b863f
FranciscoAlva <FALVAREZR91@alumnos.imf.com>
2024-02-18 02:33:40 +0100

    Crear Registro

--- discos.py
+++ discos.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 #menu
 def menu():
     print("Escoge una opcion: ")
@@ -11,6 +13,16 @@ def menu():
     #if elif
     if opcion == "1":
         print("Insertamos un registro")
+        artista = input("Introduce el artista: ")
+        anio = input("Introduce el año: ")
+        titulo = input("Introduce el título: ")
+        conexion = sqlite3.connect("discos.db")
+        cursor = conexion.cursor()
+        peticion = "INSERT INTO discos VALUES (NULL,'"+artista+"',"+str(anio)+",'"+titulo+"')"
+        cursor.execute(peticion)
+        conexion.commit()
+        conexion.close()
+        print("Tu resgistro de guardo correctamente")
     elif opcion == "2":
         print("Mostramos los registros")
     elif opcion == "3":
@@ -21,6 +33,7 @@ def menu():
         print("Buscamos los registros")
     elif opcion == "6":
         print("Salimos del programa")
+    #menurecursivo
     menu()
 menu()
     
