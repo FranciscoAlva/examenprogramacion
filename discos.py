8d287ed2141414bf359c638ec5739da45beaea51
FranciscoAlva <FALVAREZR91@alumnos.imf.com>
2024-02-18 02:00:32 +0100

    Menu Inicial

---
+++ discos.py
@@ -0,0 +1,13 @@
+def menu():
+    print("Escoge una opcion: ")
+    print("1.-Insertar un registro")
+    print("2.-Mostrar registros")
+    print("3.-Modificar un registro")
+    print("4.-Eliminar un registro")
+    print("5.-Buscar un registro")
+    print("6.-Salir")
+    input("Escoge una opcion: ")
+    menu()
+
+menu()
+    
